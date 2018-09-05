2e9616716eedff8938dacfd5cef546130de73140
alakijayo <alakijaayo@gmail.com>
2018-09-05 15:02:59 +0100

    boris bikes challenge 8

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,5 +1,10 @@
 require './lib/dockingstation'
 
 describe DockingStation do
-
+  it { is_expected.to respond_to :release_bike}
+=begin
+  it 'has "release_bike" method' do
+    expect(DockingStation.new).to respond_to(:release_bike)
+  end
+=end
 end
