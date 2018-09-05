3993b31d602574c33df3372be3a789c70831b927
Andres Faraone <andres@Andress-MacBook-Air.local>
2018-09-05 16:55:46 +0100

    Created dock method for DockingStation

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,8 +1,11 @@
 require './lib/bike'
 
 class DockingStation
+  attr_reader :docked_bikes
   def release_bike
     Bike.new
   end
-
+  def dock(bike)
+    @docked_bikes = bike
+  end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,9 +3,15 @@ require './lib/bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike}
+  it { is_expected.to respond_to :dock }
   it '"release_bike" returns a "Bike" object' do
     bike = subject.release_bike
     expect(bike).to be_working
     #expect(subject.release_bike).to be_a(Bike)
+  end
+  it "stores a bike object when docked" do
+    bike123 = Bike.new
+    subject.dock(bike123)
+    expect(subject.docked_bikes).to eq bike123
   end
 end
