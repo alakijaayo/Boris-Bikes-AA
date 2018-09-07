269990813d800aca1e089f6512951967faddcd6f
alakijayo <alakijaayo@gmail.com>
2018-09-07 21:08:20 +0100

    Releasing Bike

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,1 +1,5 @@
-
+class Bike
+  def working?
+    true
+  end
+end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,7 +1,11 @@
 require './lib/dockingstation'
+require './lib/bike'
 
 describe DockingStation do
-
   it { is_expected.to respond_to :release_bike }
 
+  it 'releases a working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
